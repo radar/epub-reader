b59022d79dd11cd359a518fe67fec9301650879d
Ryan Bigg <me@ryanbigg.com>
2020-06-07 16:20:45 +1000

    Handle paths with anchors in them

--- lib/epub-reader/page.rb
+++ lib/epub-reader/page.rb
@@ -1,13 +1,14 @@
 module Epub
   class Page
+    attr_reader :title, :path, :anchor, :file
     def initialize(title, path, file)
       @title = title
-      @path  = path
+      @path, @anchor = path.split("#")
       @file  = file
     end
-    
+
     attr_reader :title, :path
-    
+
     def content
       @content ||= get_page_content
     end
@@ -15,12 +16,12 @@ module Epub
     private
 
     def get_page_content
-      begin
-        @file.get_input_stream(@path).read.force_encoding(Encoding::UTF_8)
-      rescue
-        ""
-      end
-    end
+      file_contents = @file.get_input_stream(@path).read
+      return file_contents unless anchor
 
+      html = Nokogiri::HTML.parse(file_contents)
+
+      html.css("##{anchor}").to_s
+    end
   end
 end
--- spec/page_spec.rb
+++ spec/page_spec.rb
@@ -3,22 +3,22 @@ require 'spec_helper'
 describe Epub::Page do
 
   before(:all) do
-    file   = 'spec/data/valid.epub'
+    file   = 'spec/data/rails.epub'
     reader = Epub::Reader.open(file)
     @toc   = Epub::Toc.new(reader.package.toc, reader)
     @page  = @toc.pages.last
   end
 
   it 'get page title' do
-    @page.title.should eq('Copyright Page')
+    @page.title.should eq('Summary')
   end
 
   it 'get page path' do
-    @page.path.should eq('OPS/copyright.xhtml')
+    @page.path.should eq('EPUB/text/ch001.xhtml')
   end
 
   it 'get page content' do
-    @page.content.should match('<html.*>')
-    @page.content.should match('Produced by Daniel Lazarus and Jonesey')
+    @page.content.should match('<section.*>')
+    @page.content.should match('In this chapter, you learned what Rails is')
   end
 end
